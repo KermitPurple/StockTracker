1211670f3c713ced976d1b06ac785d8d29ce2a20
Shane McDonough <shanemcdman@gmail.com>
2020-01-09 22:10:49 -0800

    Complete print_data and prototype get_data

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from openpyxl import load_workbook
 
 def get_data():
-    return [
-            [11, 12, 13, 14, 15],
-            [21, 22, 23, 24, 25],
-            [31, 32, 33, 34, 35],
-            [41, 42, 43, 44, 45],
-            [51, 52, 53, 54, 55]
-            ]
+    print("Getting Data ..." , end  = " ")
+    tags = ["TSLA", "MJNA", "PLUG"]
+    big_list = [["date","Company","Tag","Price","Number of Shares","total"]]
+    for i, tag in enumerate(tags):
+        inner_list = [str(i) + '1',str(i) + '2',str(i) + '3',str(i) + '4',]
+        big_list.append(inner_list)
+    print("Complete")
+    return big_list
 
 def print_data(big_list):
+    print("Printing Data ...", end = " ")
     wb = load_workbook("sheet.xlsx")
     wa = wb.active
     for item in big_list:
         wa.append(item)
     wb.save("sheet.xlsx")
+    print("Complete")
 
 data = get_data()
 print_data(data)
